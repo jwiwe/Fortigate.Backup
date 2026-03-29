1232aaeaabe257c031fa25a52cc2706ab7239f32
Jonas Wiwe <jonas@wiwe.org>
2026-03-29 17:01:30 +0200

    Implement Spectre.Console and remove CommandLineParser

--- Fortigate.Backup.Cli/Program.cs
+++ Fortigate.Backup.Cli/Program.cs
@@ -1,8 +1,8 @@
-﻿using CommandLine;
-using Fortigate.Backup.Core;
+﻿using Fortigate.Backup.Core;
 using Fortigate.Backup.Core.Models;
 using Microsoft.Extensions.Configuration;
-using static Fortigate.Backup.Cli.CommandLineOptions;
+using Spectre.Console;
+using File = System.IO.File;
 
 namespace Fortigate.Backup.Cli
 {
@@ -10,159 +10,301 @@ namespace Fortigate.Backup.Cli
     {
         static async Task Main(string[] args)
         {
+            Console.Clear();
             SqliteDataAccess.InitializeDatabase();
             IConfiguration configuration = ConfigHelper.GetConfig();
-            ValidateKey.EnsureKeyExists();
-            if(!ValidateKey.EnsureKeyExists())
+            if (!ValidateKey.EnsureKeyExists())
             {
                 const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#%&/()=?";
                 var random = new Random();
                 string newKey = new string(Enumerable.Range(1, 32).Select(_ => chars[random.Next(chars.Length)]).ToArray());
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("!!! SECURITY ERROR !!!");
-                Console.WriteLine("No encryption key (environment variable) found.");
-                Console.WriteLine("Please set the environment variable 'Fortigate_Backup__SecretKey' to a secure 32 byte string value before running the program.");
-                Console.WriteLine("The program is being terminated to protect your data.");
-                Console.WriteLine("");
-                Console.Write($"Suggested key: ");
-                Console.ForegroundColor= ConsoleColor.Blue;
-                Console.WriteLine($"{newKey}");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("");
-                Console.WriteLine("Windows:");
-                Console.Write($"setx Fortigate_Backup__SecretKey \"");
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.Write($"{newKey}");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("\"");
-                Console.WriteLine("RefreshEnv");
-                Console.WriteLine("");
-                Console.WriteLine("Linux/macOS:");
-                Console.Write($"export Fortigate_Backup__SecretKey=\"");
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.Write($"{newKey}");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("\"");
-                Console.ResetColor();
+                AnsiConsole.MarkupLine("[red bold]!!! SECURITY ERROR !!![/]");
+                AnsiConsole.MarkupLine("[red]No encryption key (environment variable) found.\n" +
+                    "Please set the environment variable 'Fortigate_Backup__SecretKey' to a secure 32 byte string value before running the program.\n" +
+                    "The program is being terminated to protect your data.[/]\n");
+                AnsiConsole.MarkupLine($"Suggested key: [blue]{newKey}[/]\n");
+                AnsiConsole.MarkupLine("[bold]Windows:[/]");
+                AnsiConsole.MarkupLine($"setx Fortigate_Backup__SecretKey \"[blue]{newKey}[/]\"\n" +
+                    $"RefreshEnv\n");
+                AnsiConsole.MarkupLine("[bold]Linux/macOS:[/]");
+                AnsiConsole.MarkupLine($"export Fortigate_Backup__SecretKey=\"[blue]{newKey}[/]\"\n" +
+                    $"RefreshEnv\n");
                 return; // Stop programmet
             }
             if (!ValidateKey.EnsureKeyIsValid())
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("!!! SECURITY ERROR !!!");
-                Console.WriteLine("Your encryption key (environment variable) does not match the database.");
-                Console.WriteLine("The program is being terminated to protect your data.");
-                Console.ResetColor();
+                AnsiConsole.MarkupLine("[red bold]!!! SECURITY ERROR !!![/]");
+                AnsiConsole.MarkupLine("[red]Your encryption key (environment variable) does not match the database.\n" +
+                    "The program is being terminated to protect your data.[/]\n");
                 return; // Stop programmet
             }
-            await Parser.Default.ParseArguments<AddOptions, ListOptions, EditOptions, DeleteOptions, BackupOptions>(args)
-                .MapResult(
-                        (AddOptions opts) => HandleAddCommand(opts),
-                        (ListOptions opts) => HandleListCommand(opts),
-                        (EditOptions opts) => HandleEditCommand(opts),
-                        (DeleteOptions opts) => HandleDeleteCommand(opts),
-                        async (BackupOptions opts) => await HandleBackupCommand(opts),
-                        errs => Task.FromResult(1)
-                    );
+
+            if(args.Length > 0)
+            {
+                string command = args[0].ToLower();
+                switch (command)
+                {
+                    case "backupall":
+                        await HandleBackupAllCommand();
+                        return;
+                    default:
+                        AnsiConsole.MarkupLine($"[red]Unknown command: {command}[/]");
+                        return;
+                }
+            }
+
+            var environments = new Dictionary<string, string>
+            {
+                { "list", "List all Fortigates in the database." },
+                { "add", "Add a new Fortigate to the database." },
+                { "edit", "Edit an existing Fortigate in the database" },
+                { "delete", "Delete a Fortigate from the database" },
+                { "backup", "Backup a Fortigate configuration" },
+                { "backupAll", "Backup all Fortigates in the database" },
+                { "exit", "Exit the program" }
+            };
+
+            do {
+                Console.Clear();
+                FigletText title = new FigletText("Fortigate Backup")
+                .Centered()
+                .Color(Color.Green);
+
+                AnsiConsole.Write(new Rule().RuleStyle(Style.Parse("green dim")));
+                AnsiConsole.Write(title);
+                AnsiConsole.Write(new Rule().RuleStyle(Style.Parse("green dim")));
+
+                var selected = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("Select a task:")
+                        .UseConverter(key => environments[key])
+                        .AddChoices(environments.Keys));
+
+                Console.Clear();
+
+                switch (selected)
+                {
+                    case "add":
+                        await HandleAddCommand();
+                        break;
+                    case "list":
+                        await HandleListCommand();
+                        break;
+                    case "edit":
+                        await HandleEditCommand();
+                        break;
+                    case "delete":
+                        await HandleDeleteCommand();
+                        break;
+                    case "backup":
+                        await HandleBackupCommand();
+                        break;
+                    case "backupAll":
+                        await HandleBackupAllCommand();
+                        Console.ReadKey();
+                        break;
+                    default:
+                        return;
+                }
+            } while (true);
         }
 
-        private static Task<int> HandleAddCommand(AddOptions opts)
+        private static Task<int> HandleAddCommand()
         {
-            // Implement your logic for the 'add' command here
+            string name = AnsiConsole.Prompt(
+                new TextPrompt<string>("Name:")
+            );
+            string ipAddress = AnsiConsole.Prompt(
+                new TextPrompt<string>("IP Address:")
+            );
+            int port = AnsiConsole.Prompt(
+                new TextPrompt<int>("Port:")
+                    .DefaultValue(443)
+            );
+            string apikey = AnsiConsole.Prompt(
+                new TextPrompt<string>("API Key:")
+                    .Secret()
+            );
             var gate = new GateModel
             {
-                Name = opts.Name,
-                IpAddress = opts.IpAddress,
-                Port = opts.Port,
-                Apikey = CryptoService.Encrypt(opts.Apikey)
+                Name = name,
+                IpAddress = ipAddress,
+                Port = port,
+                Apikey = CryptoService.Encrypt(apikey)
             };
 
             SqliteDataAccess.SaveGate(gate);
 
-            Console.WriteLine($"Adding device: {opts.Name} with IP: {opts.IpAddress}");
+            AnsiConsole.MarkupLine($"Device added: [blue]{name}[/] with IP: [blue]{ipAddress}[/] on Port: [blue]{port}[/]");
             return Task.FromResult(0);
         }
 
-        private static Task<int> HandleListCommand(ListOptions opts)
+        private static Task<int> HandleListCommand()
         {
             // Implement your logic for the 'list' command here
-            Console.WriteLine("All Fortigates:");
+            AnsiConsole.MarkupLine("[bold]All Fortigates:[/]");
+            Table table = new Table();
+            table.AddColumn("[bold]ID[/]");
+            table.AddColumn("[bold]Name[/]");
+            table.AddColumn("[bold]IP Address[/]");
+            table.AddColumn("[bold]Port[/]");
+
             var gates = SqliteDataAccess.LoadGates();
             foreach (var gate in gates)
             {
-                Console.WriteLine($"{gate.Id}\t{gate.Name}\t{gate.IpAddress}\t{gate.Port}");
+                table.AddRow(gate.Id.ToString(), gate.Name, gate.IpAddress, gate.Port.ToString());
             }
+            AnsiConsole.Write(table);
+            Console.ReadKey();
             return Task.FromResult(0);
         }
 
-        private static Task<int> HandleEditCommand(EditOptions opts)
+        private static Task<int> HandleEditCommand()
         {
-            // Implement your logic for the 'list' command here
-            var gate = SqliteDataAccess.LoadGateById(opts.Id);
+            int id = ShowSelectionList("Select Fortigate to edit:");
+
+            if(id <= 0) return Task.FromResult(0);
+
+            var gate = SqliteDataAccess.LoadGateById(id);
+
             if (gate == null)
             {
-                Console.WriteLine($"Device with ID {opts.Id} not found.");
+                Console.WriteLine($"Device with ID {id} not found.");
+                Console.ReadKey();
                 return Task.FromResult(1);
             }
-            if (opts.Name != null) gate.Name = opts.Name;
-            if (opts.IpAddress != null) gate.IpAddress = opts.IpAddress;
-            if (opts.Port != null) gate.Port = opts.Port;
-            if (opts.Apikey != null) gate.Apikey = CryptoService.Encrypt(opts.Apikey);
+
+            string name = AnsiConsole.Prompt(
+                new TextPrompt<string>("Name:")
+                .DefaultValue(gate.Name)
+            );
+            string ipAddress = AnsiConsole.Prompt(
+                new TextPrompt<string>("IP Address:")
+                .DefaultValue(gate.IpAddress)
+            );
+            int port = AnsiConsole.Prompt(
+                new TextPrompt<int>("Port:")
+                    .DefaultValue((int)gate.Port)
+            );
+            string apikey = AnsiConsole.Prompt(
+                new TextPrompt<string>("API Key:")
+                    .Secret()
+                    .AllowEmpty()
+            );
+            
+            gate.Name = name;
+            gate.IpAddress = ipAddress;
+            gate.Port = port;
+            if (apikey != null && apikey != "") gate.Apikey = CryptoService.Encrypt(apikey);
             SqliteDataAccess.UpdateGate(gate);
-
+            AnsiConsole.MarkupLine($"\nDevice updated");
+            Console.ReadKey();
             return Task.FromResult(0);
         }
 
-        private static Task<int> HandleDeleteCommand(DeleteOptions opts)
+        private static Task<int> HandleDeleteCommand()
         {
-            // Implement your logic for the 'delete' command here
-            var gate = SqliteDataAccess.LoadGateById(opts.Id);
+            int id = ShowSelectionList("Select Fortigate to delete:");
+
+            if (id <= 0) return Task.FromResult(0);
+
+            var gate = SqliteDataAccess.LoadGateById(id);
+
             if (gate == null)
             {
-                Console.WriteLine($"Device with ID {opts.Id} not found.");
+                Console.WriteLine($"Device with ID {id} not found.");
+                Console.ReadKey();
                 return Task.FromResult(1);
             }
-            SqliteDataAccess.DeleteGate(gate.Id);
+
+            var selected = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title($"Are you sure you want to delete this device '{gate.Name} ({gate.IpAddress})'?")
+                .AddChoices(new[] { "No", "Yes" }));
+
+            if(selected == "Yes")
+            {
+                SqliteDataAccess.DeleteGate(gate.Id);
+                AnsiConsole.MarkupLine($"\nDevice deleted");
+                Console.ReadKey();
+            }
             return Task.FromResult(0);
         }
 
-        private static async Task<int> HandleBackupCommand(BackupOptions opts)
+        private static async Task<int> HandleBackupCommand()
         {
-            var createText = string.Empty;
-            if (opts.Id <= 0)
+            var list = new Dictionary<int, string>();
+            var gates = SqliteDataAccess.LoadGates();
+            foreach (var gate in gates)
             {
-                var gates = SqliteDataAccess.LoadGates();
-                foreach (var item in gates)
+                list.Add(gate.Id, $"{gate.Name} ({gate.IpAddress})");
+            }
+
+            var selected = AnsiConsole.Prompt(
+                new MultiSelectionPrompt<int>()
+                    .Title("title")
+                    .UseConverter(key => list[key])
+                    .AddChoices(list.Keys)
+                    .PageSize(24));
+
+            foreach (var item in selected)
+            {
+                var gate = SqliteDataAccess.LoadGateById(item);
+                string createText = await BackupGate.Backup(gate);
+                if (createText == null)
                 {
-                    createText = await BackupGate.Backup(item);
-                    if (createText == null)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"Unable to backup the device with ID {item.Id}");
-                        Console.ResetColor();
-                        continue;
-                    }
-                    File.WriteAllText($"Backups\\{item.Name}_{DateTime.Now.ToString("dd-MM-yyyy_HHmm")}.txt", createText);
-                    Console.WriteLine($"Backuped device with ID: {item.Id}");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Unable to backup the device with ID {gate.Id}");
+                    Console.ResetColor();
+                    continue;
                 }
-                return 0;
+                File.WriteAllText($"Backups\\{gate.Name}_{DateTime.Now.ToString("dd-MM-yyyy_HHmm")}.txt", createText);
+                Console.WriteLine($"Backuped the device: {gate.Name} ({gate.IpAddress})");
             }
-            var gate = SqliteDataAccess.LoadGateById(opts.Id);
-            if (gate == null)
-            {
-                Console.WriteLine($"Device with ID {opts.Id} not found.");
-                return 1;
-            }
-            createText = await BackupGate.Backup(gate);
-            if (createText == null)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Unable to backup the device with ID {gate.Id}");
-                Console.ResetColor();
-                return 1;
-            }
-            File.WriteAllText($"Backups\\{gate.Name}_{DateTime.Now.ToString("dd-MM-yyyy_HHmm")}.txt", createText);
+            AnsiConsole.MarkupLine("\nAll selected devices have been processed.");
+            Console.ReadKey();
             return 0;
+        }
+
+        private static async Task<int> HandleBackupAllCommand()
+        {
+            var gates = SqliteDataAccess.LoadGates();
+            foreach (var gate in gates)
+            {
+                string createText = await BackupGate.Backup(gate);
+                if (createText == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Unable to backup the device with ID {gate.Id}");
+                    Console.ResetColor();
+                    continue;
+                }
+                File.WriteAllText($"Backups\\{gate.Name}_{DateTime.Now.ToString("dd-MM-yyyy_HHmm")}.txt", createText);
+                Console.WriteLine($"Backuped the device: {gate.Name} ({gate.IpAddress})");
+            }
+            AnsiConsole.MarkupLine("\nAll devices have been processed.");
+            return 0;
+        }
+
+        private static int ShowSelectionList(string title = "Select Fortigate:")
+        {
+            var list = new Dictionary<int, string>();
+            var gates = SqliteDataAccess.LoadGates();
+            list.Add(0, "[red]Exit[/]");
+            foreach (var gate in gates)
+            {
+                list.Add(gate.Id, $"{gate.Name} ({gate.IpAddress})");
+            }
+            list.Add(-1, "[red]Exit[/]");
+
+            var selected = AnsiConsole.Prompt(
+                new SelectionPrompt<int>()
+                    .Title(title)
+                    .UseConverter(key => list[key])
+                    .AddChoices(list.Keys)
+                    .PageSize(24));
+
+            return selected;
         }
     }
 }
